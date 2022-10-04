3f8224b87ff95649f9b5d0b658f6003361175407
Ercüment Kalkan <ercukalkann@gmail.com>
2022-10-04 18:55:46 +0300

    IPC example

--- renderer.js
+++ renderer.js
@@ -1,2 +1,10 @@
 const information = document.getElementById('info')
 information.innerText = `This app is using Chrome (v${versionsaq.chrome()}), Node.js (v${versionsaq.node()}), and Electron (v${versionsaq.electron()})`
+
+const func = async () => {
+    debugger;
+    const response = await window.versionsaq.ping();
+    console.log(response);
+}
+
+func();
